34c5b1605d1eb899a657376964aac328c034095e
DennerLuan <denner.luan@prox3.com.br>
2013-05-25 12:40:59 -0300

    SEO

--- V3/portfolio/meta.php
+++ V3/portfolio/meta.php
@@ -1,6 +1,21 @@
 <meta charset="UTF-8">
-<title>Untitled Document</title>
+<meta name="description" content="Veja todos os nossos trabalhos desenvolvidos." />
+<link rel="SHORTCUT ICON" href="http://www.prox3.com.br/images/favicon.png">
+<meta name="keywords" content="Prox3, produtora, produtora digital,sites, sistemas web, desenvolvimento web, portfolio">
+<meta name="title" content="Prox3 - Portfolio">
+<meta name="organization name" content="Prox3" />
+<meta http-equiv="content-language" content="pt-br" />
+<meta name="language" content="portuguese" />
+<meta name="location" content="Brazil" />
+<meta name="robots" content="index, follow" />
+<meta http-equiv="expires" content="-1" />
+<meta name="mssmarttagspreventparsing" content="true" />
+<meta http-equiv="imagetoolbar" content="no" />
+
+<meta property="og:url" content="http://prox3.com.br/portfolio/">
+<meta property="og:title" content="Prox3 - Portfolio">
+<meta property="og:type" content="Site">
+<meta property="og:image" content="http://prox3.com.br/images/logo.png">
+    
+    <title>Prox3 - Portfolio</title>
 <link rel="stylesheet" type="text/css" href="css/print.css" media="print" />
-<!--[if IE]> <link href="css/ie.css" media="screen" rel="stylesheet" type="text/css" ><![endif]-->
-<link href="css/screen.css" media="screen" rel="stylesheet" type="text/css" >
-<link href="css/css.css" media="screen" rel="stylesheet" type="text/css" >
