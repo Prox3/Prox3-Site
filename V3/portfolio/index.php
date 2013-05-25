d6070d1f1039792497cd1de5abe1330441571f07
DennerLuan <denner.luan@prox3.com.br>
2013-05-25 01:11:28 -0300

    Portfolio

---
+++ V3/portfolio/index.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html>
+<head>
+<?php include 'meta.php'; ?>
+</head>
+
+<body>
+	<!-- Header -->
+  <div id="struct-header">
+  	<div id="header">
+    	<header>
+ 				<a id="logo_header" title="Home" href="//prox3.com.br">Prox3</a>
+        <ul id="navigator">
+         <li><a id="menuServicos" target="_self" href="#">Serviços</a></li>
+          <li><a id="menuClientes" target="_self" href="#">Clientes</a></li>
+          <li><a id="menuContato" target="_self" href="#">Contato</a></li>
+          <li>|</li>
+          <!--<li><a target="_self" href="portfolio.php">Portfolio</a></li>
+          <li><a target="_self" href="blog.php">Blog</a></li>-->
+          <li style="margin-right:0"><a target="_self" href="https://www.facebook.com/prox3.digital">Facebook</a></li>
+        </ul>
+      </header>
+    </div>
+	</div>
+  <!-- Fim header -->
+  <div id="tudo">
+  	<div class="text_portfolio left"><h1>PORTFOLIO</h1></div>
+    <ul id="sub_navigator" class="left">
+    	<li id="item-1">TODOS</li>
+      <li id="item-2">SITES</li>
+      <li id="item-3">WORDPRESS</li>
+      <li id="item-4">APP FACEBOOK</li>
+      <li id="item-5">MOBILE</li>
+      <li id="item-6">SISTEMAS</li>
+    </ul>
+    <img src="imagens/img_port.jpg" width="200" height="200" alt="" title="">
+    <img src="imagens/img_port2.jpg" width="200" height="200" alt="" title="">
+    <img src="imagens/img_port3.jpg" width="200" height="200" alt="" title="">
+    <img src="imagens/img_port.jpg" width="200" height="200" alt="" title="">
+    <img src="imagens/img_port2.jpg" width="200" height="200" alt="" title="">
+    
+    <div class="body_info left">
+    	<div class="shadow_info"></div>
+      <div class="left_info left">
+      	<span class="tag_job left">SITES</span>
+        <span class="name_company left">Nome da empresa <br>2013</span>
+        <span class="tecnologic_use left"><strong>Tecnologia utilizada: </strong>PHP, Jquery, HTML5, CSS3, Wordpress, .NET, iOS, Android, SDK PHP (Facebook), SEO</span>
+        <a target="_blank" href="" class="link_company left">nomedaempresa.com.br</a>
+      </div>
+      <div class="comment_client right">
+      	<div class="text_comment left">
+      	“Ddashhhhdsa jjjdashhjd dasndasjdj sadasljf ljfcsj dasjdjsaj dasjkjdsa, dashhf djfhdhahsadjjh dans dnas dn fasndjf djhhfjds fajsjdsa afsdjadsj, dnasjf sndajhdaj danndjas ndsnkdan dsnjd nbfdkfshjf dnfsdjfjdn asndnh, sdahjsaj dsahdsad. Daskdsadh dsabdsakhd sa dsah, dsandasj, dashhdajh hsd ha dsahji dhhoawe fophjgdas idai fsdhif sdnoifadjdjkf fdshfhjdsh nvndfkhgf fjd”.
+        </div>
+        <span class="name_client left">Nome do cliente</span>
+        <span class="name_company_comment left"><strong>Nome da empresa</strong></span>
+      </div>
+    </div>
+    
+    <img src="imagens/img_port2.jpg" width="200" height="200" alt="" title="">
+    <img src="imagens/img_port3.jpg" width="200" height="200" alt="" title="">
+    <img src="imagens/img_port.jpg" width="200" height="200" alt="" title="">
+    <img src="imagens/img_port2.jpg" width="200" height="200" alt="" title="">
+    <img src="imagens/img_port3.jpg" width="200" height="200" alt="" title="">
+    
+    <img src="imagens/img_port.jpg" width="200" height="200" alt="" title="">
+    <img src="imagens/img_port2.jpg" width="200" height="200" alt="" title="">
+    <img src="imagens/img_port3.jpg" width="200" height="200" alt="" title="">
+    <img src="imagens/img_port.jpg" width="200" height="200" alt="" title="">
+    <img src="imagens/img_port2.jpg" width="200" height="200" alt="" title="">
+    
+    <img src="imagens/img_port2.jpg" width="200" height="200" alt="" title="">
+    <img src="imagens/img_port3.jpg" width="200" height="200" alt="" title="">
+    <img src="imagens/img_port.jpg" width="200" height="200" alt="" title="">
+    <img src="imagens/img_port2.jpg" width="200" height="200" alt="" title="">
+    <img src="imagens/img_port3.jpg" width="200" height="200" alt="" title="">
+  </div>
+  <div id="footer">
+    &copy; COPYRIGHT Prox3 Produtora Digital
+  </div>
+</body>
+</html>
---
+++ V3/portfolio/meta.php
@@ -0,0 +1,6 @@
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+<link rel="stylesheet" type="text/css" href="css/print.css" media="print" />
+<!--[if IE]> <link href="css/ie.css" media="screen" rel="stylesheet" type="text/css" ><![endif]-->
+<link href="css/screen.css" media="screen" rel="stylesheet" type="text/css" >
+<link href="css/css.css" media="screen" rel="stylesheet" type="text/css" >
